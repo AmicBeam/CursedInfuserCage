a49080ae1ad595f9972b68496ae5f7aa9f85c948
amicbeam <1425257712@qq.com>
2025-11-05 02:18:40 +0800

    chore: update version and code optimization

--- src/main/java/com/cursedinfuserbycage/CursedInfuserByCage.java
+++ src/main/java/com/cursedinfuserbycage/CursedInfuserByCage.java
@@ -1,14 +1,13 @@
 package com.cursedinfuserbycage;
 
 import com.cursedinfuserbycage.config.ModConfig;
-import com.cursedinfuserbycage.ponder.CursedInfuserByCagePonderPlugin;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig.Type;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.createmod.ponder.foundation.PonderIndex;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -22,13 +21,31 @@ public class CursedInfuserByCage {
         ModLoadingContext.get().registerConfig(Type.COMMON, ModConfig.SPEC);
         
         MinecraftForge.EVENT_BUS.register(this);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+        
+        // 只有当 Ponder 模组存在时才注册 Ponder 场景
+        if (ModList.get().isLoaded("ponder")) {
+            FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+            LOGGER.info("Ponder detected, enabling Ponder integration");
+        }
+        
         LOGGER.info("Cursed Infuser By Cage mod loaded!");
     }
     
     private void doClientStuff(final FMLClientSetupEvent event) {
+        // 使用反射动态加载 Ponder 插件，避免编译时依赖
         event.enqueueWork(() -> {
-            PonderIndex.addPlugin(new CursedInfuserByCagePonderPlugin());
+            try {
+                Class<?> ponderIndexClass = Class.forName("net.createmod.ponder.foundation.PonderIndex");
+                Class<?> pluginClass = Class.forName("com.cursedinfuserbycage.ponder.CursedInfuserByCagePonderPlugin");
+                
+                Object plugin = pluginClass.getDeclaredConstructor().newInstance();
+                var addPluginMethod = ponderIndexClass.getMethod("addPlugin", Class.forName("net.createmod.ponder.api.registration.PonderPlugin"));
+                addPluginMethod.invoke(null, plugin);
+                
+                LOGGER.info("Successfully registered Ponder plugin");
+            } catch (Exception e) {
+                LOGGER.warn("Failed to register Ponder plugin (this is normal if Ponder is not installed): " + e.getMessage());
+            }
         });
     }
 }
